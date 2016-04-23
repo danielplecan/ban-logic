73ebe8c73f354ab42a708503ece35278f48736b4
Alexandru Buzneanu <bznnalex@gmail.com>
2016-04-23 23:13:44 +0300

    Add logging info lvl

--- src/main/java/org/banlogic/main/Program.java
+++ src/main/java/org/banlogic/main/Program.java
@@ -1,5 +1,10 @@
 package org.banlogic.main;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 /**
@@ -7,7 +12,14 @@ import java.io.IOException;
  */
 public class Program {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Program.class);
+
     public static void main(String... args) {
+        BasicConfigurator.configure();
         //TODO init point
+        if(args.length < 1){
+            LOGGER.info("Input format: app.jar filename.txt");
+            return;
+        }
     }
 }
