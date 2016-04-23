dbf09385e9e3c8c897b998b1a738af76fea3d5a5
Alexandru Buzneanu <bznnalex@gmail.com>
2016-04-23 22:06:21 +0300

    Build jar with dep using declared main class

---
+++ src/main/java/org/banlogic/main/Program.java
@@ -0,0 +1,13 @@
+package org.banlogic.main;
+
+import java.io.IOException;
+
+/**
+ * Created by Alexandru on 4/23/2016.
+ */
+public class Program {
+
+    public static void main(String... args) {
+        //TODO init point
+    }
+}
